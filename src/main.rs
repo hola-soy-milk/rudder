db2c8a703a0b1397c8c12ec94d7d040588c6254e
Ramon Huidobro <hola@ramonhuidobro.com>
2020-05-03 16:47:56 +0200

    Download stuff from URL

--- src/main.rs
+++ src/main.rs
@@ -11,4 +11,15 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
     println!("url: {}", args.url);
+    let result = reqwest::blocking::get(&args.url);
+    let text_result = match result {
+        Ok(text_result) => { text_result }
+        Err(error) => { panic!("Can't deal with {}, just exit here", error); }
+    };
+
+    let text = match text_result.text() {
+        Ok(text) => { text }
+        Err(error) => { panic!("Can't deal with {}, just exit here", error); }
+    };
+    println!("{}", text);
 }
