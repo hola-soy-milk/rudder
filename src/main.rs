c5f78ee191537c9d4a6ee8d4c3576c6295a8ecdf
Ramon Huidobro <hola@ramonhuidobro.com>
2020-05-06 22:19:58 +0200

    Add xml dep and start parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use structopt::StructOpt;
+use std::io::BufReader;
+
+use xml::reader::{EventReader, XmlEvent};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -6,6 +9,12 @@ use structopt::StructOpt;
 struct Cli {
     /// The URL
     url: String,
+}
+
+fn indent(size: usize) -> String {
+    const INDENT: &'static str = "    ";
+    (0..size).map(|_| INDENT)
+             .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
 }
 
 fn main() {
@@ -21,5 +30,21 @@ fn main() {
         Ok(text) => { text }
         Err(error) => { panic!("Can't deal with {}, just exit here", error); }
     };
-    println!("{}", text);
+    let file = BufReader::new(text.as_bytes());
+    let parser = EventReader::new(file);
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{}", name);
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                println!("{}", name);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
 }
